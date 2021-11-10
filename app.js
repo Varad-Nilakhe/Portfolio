a055e759a813be9ddfb1c1b8e1273d28b44a82a4
Varad57 <80471728+Varad57@users.noreply.github.com>
2021-11-10 11:00:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// Custom cursor
+
 new kursor({
   type: 1,
   removeDefaultCursor: true,
   color: "#ff61e5",
 });
+
+// skeww
+
+// Smooth scrolling
 
 const body = document.body,
   scrollWrap = document.getElementsByClassName("first")[0],
@@ -24,6 +30,8 @@ function smoothScroll() {
 
 smoothScroll();
 
+// Animation
+
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
@@ -34,6 +42,32 @@ let scene = new ScrollMagic.Scene({
   duration: "300%",
   triggerHook: 0,
 })
+
   .setTween(timeline)
   .setPin("nav .nav-div")
   .addTo(controller);
+
+let proxy = { skew: 0 },
+  skewSetter = gsap.quickSetter(".welcome-text", "skewY", "deg"), // fast
+  clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / -300);
+    console.log(skew);
+    // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {
+        skew: 0,
+        duration: 0.5,
+        ease: "power3",
+        overwrite: true,
+        onUpdate: () => skewSetter(proxy.skew),
+      });
+    }
+  },
+});
+
+// make the right edge "stick" to the scroll bar. force3D: true improves performance
+gsap.set(".skewElem", { transformOrigin: "right center", force3D: true });
